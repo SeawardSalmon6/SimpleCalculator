0560c27dfb59839cb205b625e01d14d6f6507b49
SeawardSalmon6 <rayhanachour@hotmail.com>
2021-10-12 18:30:11 -0300

    Making the calculator functions and modeling HTML

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-const calcScreen = document.getElementById('calcScreen');
+const calcScreen = document.getElementById('calc-screen');
 const keys = document.querySelectorAll(".key");
 
-keys.length
+for(let el of keys)
+    el.addEventListener("click", event => { writeOnScreen(el.textContent) });
+
+function writeOnScreen(val) {
+    let valOnScreen = calcScreen.textContent;
+    switch(val) {
+        case "X":
+        case "x":
+            calcScreen.textContent += "*";
+            break;
+
+        case "C":
+            calcScreen.textContent = "";
+            break;
+
+        case "<<":
+            calcScreen.textContent = valOnScreen.substring(0, valOnScreen.length - 1);
+            break;
+
+        case "=":
+            if(!valOnScreen == "") calcScreen.textContent = eval(valOnScreen);
+            else calcScreen.textContent = "No data to evaluate";
+            break;
+
+        default:
+            calcScreen.textContent += val;
+            break;
+    }
+}
